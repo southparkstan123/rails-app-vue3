36598cd2c495a1f5e0754386c0c666dd00a37c15
southparkstan123 <southparkstan123@gmail.com>
2025-03-21 15:03:48 +0800

    Change mysql to postgres as the default database and update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,64 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_15_115115) do
+ActiveRecord::Schema[7.2].define(version: 2023_08_15_115042) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
-  create_table "authors", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.bigint "creator_id"
-    t.bigint "updater_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["creator_id"], name: "index_authors_on_creator_id"
-    t.index ["updater_id"], name: "index_authors_on_updater_id"
-  end
-
-  create_table "authors_books", id: false, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.bigint "author_id"
-    t.bigint "book_id"
-    t.index ["author_id"], name: "index_authors_books_on_author_id"
-    t.index ["book_id"], name: "index_authors_books_on_book_id"
-  end
-
-  create_table "books", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "name"
-    t.text "abstract"
-    t.decimal "price", precision: 5, scale: 2
-    t.bigint "creator_id"
-    t.bigint "updater_id"
-    t.bigint "publisher_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["creator_id"], name: "index_books_on_creator_id"
-    t.index ["publisher_id"], name: "index_books_on_publisher_id"
-    t.index ["updater_id"], name: "index_books_on_updater_id"
-  end
-
-  create_table "publishers", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.bigint "creator_id"
-    t.bigint "updater_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["creator_id"], name: "index_publishers_on_creator_id"
-    t.index ["updater_id"], name: "index_publishers_on_updater_id"
-  end
-
-  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
     t.string "password_digest"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
-
-  add_foreign_key "authors", "users", column: "creator_id"
-  add_foreign_key "authors", "users", column: "updater_id"
-  add_foreign_key "books", "publishers", on_delete: :cascade
-  add_foreign_key "books", "users", column: "creator_id"
-  add_foreign_key "books", "users", column: "updater_id"
-  add_foreign_key "publishers", "users", column: "creator_id"
-  add_foreign_key "publishers", "users", column: "updater_id"
 end
